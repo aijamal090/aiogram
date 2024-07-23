5ac6e503edfa31ba9deb24af9b311b655d1f4a5f
aijamal090 <mwahmoha@gmail.com>
2024-07-23 11:08:45 +0600

    lesson_6.py

--- lesson_6.py
+++ lesson_6.py
@@ -95,3 +95,75 @@ print(math == math_2)
 print(math != math_2)
 print(math >= math_2)
 print(math <= math_2)
+class MagicCalculator:
+    def __init__(self, number_1, number_2):
+        self.number_1 = number_1
+        self.number_2 = number_2
+    
+    def __add__(self, other):
+        return self.number_1 + other.number_1, self.number_2 + other.number_2
+    
+    def __sub__(self, other):
+        return self.number_1 - other.number_1, self.number_2 - other.number_2
+    
+    def __mul__(self, other):
+        return self.number_1 * other.number_1, self.number_2 * other.number_2
+    
+    def __truediv__(self, other):
+        return self.number_1 / other.number_1, self.number_2 / other.number_2
+    
+    def __floordiv__(self, other):
+        return self.number_1 // other.number_1, self.number_2 // other.number_2
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+    def __le__(self, other):
+        return self.year <= other.year
+
+    def __gt__(self, other):
+        return self.year > other.year
+
+    def __ge__(self, other):
+        return self.year >= other.year
+
+    def __eq__(self, other):
+        return self.year == other.year
+
+    def __ne__(self, other):
+        return self.year != other.year
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+    def __le__(self, other):
+        return self.year <= other.year
+
+    def __gt__(self, other):
+        return self.year > other.year
+
+    def __ge__(self, other):
+        return self.year >= other.year
+
+    def __eq__(self, other):
+        return self.year == other.year
+
+    def __ne__(self, other):
+        return self.year != other.year
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
+
